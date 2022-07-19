b45bfe8a99f7ff04851c44121a80c54bdc96497e
Nitin Nagaraj <nnitin91@gmail.com>
2022-07-19 19:36:29 +0530

    3

---
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -0,0 +1,36 @@
+// LECTURE: Values and Variables
+
+let country = "india";
+let continent = "asia";
+let population = 10000000000;
+
+console.log(
+  "Population of " +
+    country +
+    "in the continent of " +
+    continent +
+    "is " +
+    population
+);
+
+// //  //// //  //// //  //// //  //// //  //// //  //// //  //
+// LECTURE: Data Types
+
+let isIsland = 5;
+let language = "kannada";
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+console.log(typeof null);
+
+// //  //// //  //// //  //// //  //// //  //// //  //// //  //
+// LECTURE: let, const and var
+
+country = 500;
+console.log(language);
+console.log(typeof country);
+
+// //  //// //  //// //  //// //  //// //  //// //  //// //  //
+// LECTURE: Basic Operators
