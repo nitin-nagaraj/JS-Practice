16635fbfb79d734714531f90559c6f26547346f3
Nitin Nagaraj <nnitin91@gmail.com>
2022-07-19 21:39:42 +0530

    abf

--- 01-Fundamentals-Part-1/starter/index.js
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -35,12 +35,12 @@ console.log(typeof country);
 // //  //// //  //// //  //// //  //// //  //// //  //// //  //
 // LECTURE: Basic Operators
 
-const finland = 6000000;
+const finland = 600000;
 country = "india";
 let newPopulation = population / 2;
 console.log(newPopulation);
 
-const belgium = 6000000;
+const belgium = 6000005;
 
 const description = console.log(
   "Population of " + country + " if it is reduced by half is " + newPopulation
@@ -71,7 +71,6 @@ console.log(bmiJohn);
 const markHigherBMI = bmiMark > bmiJohn;
 console.log(markHigherBMI);
 
-
 // //  //// //  //// //  //// //  //// //  //// //  //// //  //
 
 // LECTURE: Strings and Template Literals
@@ -88,4 +87,41 @@ console.log(`Weight difference of mark and john is ${massJohn - massMark}`);
 
 // LECTURE: Taking Decisions: if / else Statements
 
+if (finland > belgium) {
+  console.log("Portugal's population is above average");
+} else if (finland >= belgium) {
+  console.log("Portugal's population is 22 million below average");
+}
+console.log("Blabla");
+
+// //  //// //  //// //  //// //  //// //  //// //  //// //  //
+
+//  Coding Challenge #2
+
+if (bmiMark < bmiJohn) {
+  console.log(
+    `John's BMI of ${bmiJohn} is higher than than of Mark's ${bmiMark} BMI`
+  );
+} else {
+  console.log(
+    `John's BMI of ${bmiJohn} is lower than than of Mark's ${bmiMark} BMI`
+  );
+}
+
+// //  //// //  //// //  //// //  //// //  //// //  //// //  //
+// LECTURE: Type Conversion and Coercion
+console.log('9' - '5');
+console.log('19' - '13' + 17);
+'123' < 57;
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+
+const year = "2020.0000";
+console.log(year)
+const year2 = Number(year * 4)
+console.log(year2/year);
+
+// //  //// //  //// //  //// //  //// //  //// //  //// //  //
+
+// LECTURE: Equality Operators: == vs. ===
 
