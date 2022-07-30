3039d3abbb152b224ff8fe19cd7602c60990d4bc
Nitin Nagaraj <nnitin91@gmail.com>
2022-07-31 00:30:59 +0530

    JS Fundamentals 2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function describeCountry (country,population,capitalCity) {
+    const temp = console.log(`${country} has a population of ${population} and it's capital city is ${capitalCity}`);
+    return country;
+}
+
+describeCountry("India",1000000000000,"Delhi")
+describeCountry("Brazil",1000000000000,"Rio")
+describeCountry("USA",1000000000000,"W.D.C")
