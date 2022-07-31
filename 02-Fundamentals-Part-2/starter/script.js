cd9c91290ed86f903fc617a1cf32457d729cd672
Nitin Nagaraj <nnitin91@gmail.com>
2022-07-31 19:15:36 +0530

    JS Fundamentals 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,10 +1,30 @@
 "use strict";
 
-function describeCountry (country,population,capitalCity) {
-    const temp = console.log(`${country} has a population of ${population} and it's capital city is ${capitalCity}`);
-    return country;
+//  // // // // // // // // // // // // // //
+// LECTURE: Functions
+
+function describeCountry(country, population, capitalCity) {
+  const temp = `${country} has a population of ${population} and it's capital city is ${capitalCity}`;
+  return temp;
 }
 
-describeCountry("India",1000000000000,"Delhi")
-describeCountry("Brazil",1000000000000,"Rio")
-describeCountry("USA",1000000000000,"W.D.C")
+const temp = console.log(describeCountry("India", 1000000000000, "Delhi"));
+const temp2 = console.log(describeCountry("Brazil", 20000000000, "Rio"));
+const temp3 = console.log(describeCountry("USA", 100000000, "W.D.C"));
+
+//  // // // // // // // // // // // // // //
+// LECTURE: Function Declarations vs. Expressions
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+const percentPopChina = percentageOfWorld1(100);
+const percentPopIndia = percentageOfWorld2(10000);
+console.log(percentPopChina, percentPopIndia);
+
+//  // // // // // // // // // // // // // //
