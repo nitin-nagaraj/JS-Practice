e5ee3fe3207dd66225280d52fefdc3d504d6a37b
Nitin Nagaraj <nnitin91@gmail.com>
2022-08-03 19:13:40 +0530

    DOM

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,23 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// Coding Challenge #1
+
+const arr = [17, 21, 23];
+const arr2 = [12,5,-5,0,4]
+
+function printForecast(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`${arr[i]}C in ${i+1} day`)
+  }
+}
+
+printForecast(arr);
+
+function printForecast2(arr2) {
+    for (let i = 0; i < arr2.length; i++) {
+      console.log(`${arr2[i]}C in ${i+1} day`)
+    }
+  }
+  
+  printForecast(arr2);
