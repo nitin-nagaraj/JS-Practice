acb1b3c0a60057bb08622a5e9e8b7f5591960d1f
Nitin Nagaraj <nnitin91@gmail.com>
2022-08-10 23:12:57 +0530

    DOM 1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,4 +4,8 @@ console.log(document.querySelector('.message').textContent)
 
 console.log(document.querySelector('.message').textContent) 
 
+document.querySelector('.message').textContent = "That's the Correct Number"
+document.querySelector('.number').textContent = 16;
+document.querySelector('.score').textContent = 25;
 
+document.querySelector('.guess').value = 13;
