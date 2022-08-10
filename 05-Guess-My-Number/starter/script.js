5ca2750f2d4ee63a2b909aa5abc3e823d82e98ba
Nitin Nagaraj <nnitin91@gmail.com>
2022-08-10 23:28:22 +0530

    DOM 73

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,11 +1,19 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent) 
+// console.log(document.querySelector('.message').textContent) 
 
-console.log(document.querySelector('.message').textContent) 
+// console.log(document.querySelector('.message').textContent) 
 
-document.querySelector('.message').textContent = "That's the Correct Number"
-document.querySelector('.number').textContent = 16;
-document.querySelector('.score').textContent = 25;
+// document.querySelector('.message').textContent = "That's the Correct Number"
+// document.querySelector('.number').textContent = 16;
+// document.querySelector('.score').textContent = 25;
 
-document.querySelector('.guess').value = 13;
+// document.querySelector('.guess').value = 13;
+
+document.querySelector('.check').addEventListener('click',function(){
+    const guess = document.querySelector('.guess').value;
+
+    if(!guess){
+        document.querySelector('.message').textContent = 'No Number Entered 🛑 '
+    }
+})
