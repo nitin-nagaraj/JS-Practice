cb373796f20420ef77dcc305cb172ec7638b08d3
Nitin Nagaraj <nnitin91@gmail.com>
2022-08-12 18:03:52 +0530

    game2

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,3 +1,30 @@
 'use strict';
 
-for (let i =0;i < )
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const button = document.querySelector('.close-modal');
+const buttonsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < buttonsOpenModal.length; i++) {
+  buttonsOpenModal[i].addEventListener('click', openModal);
+}
+
+button.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
