95452f62897d20e6256bac9ffe1cbcff81b0f438
Nitin Nagaraj <nnitin91@gmail.com>
2022-08-20 22:39:00 +0530

    game2

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,13 +2,14 @@
 
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.getElementById('score--1');
-const current0Element = document.getElementById('current--0')
-const current1Element = document.getElementById('current--1')
+const current0Element = document.getElementById('current--0');
+const current1Element = document.getElementById('current--1');
 const diceElement = document.querySelector('.dice');
 const buttonNewGame = document.querySelector('.btn--new');
 const buttonRollDice = document.querySelector('.btn--roll');
 const buttonHold = document.querySelector('.btn--hold');
 
+const score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
@@ -23,15 +24,15 @@ const diceRoll = function () {
   diceElement.src = `dice-${dice}.png`;
   document.querySelector('.current-score').textContent = dice;
 
-  if(dice !== 1){
+  if (dice !== 1) {
     currentScore += dice;
     current0Element.textContent = currentScore;
-  }else{
-
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
   }
 };
 
-buttonRollDice.addEventListener('click', diceRoll);
-buttonRollDice.addEventListener('click', diceRoll);
-buttonRollDice.addEventListener('click', diceRoll);
 buttonRollDice.addEventListener('click', diceRoll);
